f2ed86fdb6910af2a71b47ae7204fba59ae755c1
Nickolaj Stjujsckij <sterkrig@home.no>
2010-11-20 21:10:00 +0300

    Add a comment to deploying script, use absolute paths and quote vars

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
-RDIR=$(pwd)/$(dirname $0)
+# "Deploys" this config pack,
+# i.e. symlink config files to those in this repo
+# backing up existing files etc.
+
+RDIR=$(dirname `readlink -fn $0`)
 
 excludes="deploy.sh green_kde.colors ir_black_cscheme.vim"
 
 function include() {
     for i in ${excludes[@]}; do
-        if [ $i == $1 ]; then
+        if [ "$i" == "$1" ]; then
             return 1
         fi
     done
@@ -14,26 +18,26 @@ function include() {
 }
 
 function symlinkit() {
-    SRC=$1
-    TRGT=$2
-    if [ -L $TRGT ]; then
+    SRC="$1"
+    TRGT="$2"
+    if [ -L "$TRGT" ]; then
             echo "Removing old symlink $TRGT..."
-        rm $TRGT
-    elif [ -e $TRGT ]; then
-        if [ -e $TRGT~ ]; then
+        rm "$TRGT"
+    elif [ -e "$TRGT" ]; then
+        if [ -e "$TRGT"~ ]; then
             echo "Both $TRGT and $TRGT~ exist, skipping..."
             return
         fi
         echo "Backing up $TRGT to $TRGT~"
-        mv $TRGT $TRGT~
+        mv "$TRGT" "$TRGT"~
     fi
-    ln -s $SRC $TRGT
+    ln -s "$SRC" "$TRGT"
 }
 
 if [[ $EUID -ne 0 ]]; then
     symlinkit "$RDIR/green_kde.colors" \
         "$HOME/.kde4/share/apps/color-schemes/green_kde.colors"
-    FILES=$(ls $RDIR)
+    FILES=$(ls "$RDIR")
 else
     FILES="vimrc zshrc"
 fi
@@ -44,8 +48,8 @@ if [ ! -d "$VIMCSDIR" ]; then
 fi
 symlinkit "$RDIR/ir_black_cscheme.vim" "$VIMCSDIR/ir_black.vim"
 
-for f in $FILES; do
-    if include $f; then
+for f in "$FILES"; do
+    if include "$f"; then
         symlinkit "$RDIR/$f" "$HOME/.$f"
     fi
 done
